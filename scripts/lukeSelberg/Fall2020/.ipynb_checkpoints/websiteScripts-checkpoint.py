25c97f6286537fe25a9ce15b4906b3b39a493ade
Luke Selberg <lukerselberg@gmail.com>
2020-11-05 13:02:56 -0600

    kmeans

--- scripts/lukeSelberg/Fall2020/.ipynb_checkpoints/websiteScripts-checkpoint.py
+++ scripts/lukeSelberg/Fall2020/.ipynb_checkpoints/websiteScripts-checkpoint.py
@@ -4,6 +4,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from sklearn.cluster import MeanShift
+from sklearn import preprocessing 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
@@ -25,13 +27,13 @@ def kmerXTable(s, a, b):
     kmers = s_hat.toarray()
     return pd.DataFrame(kmers,columns=kmerNames, index = s.index)
     
-def kmeans(fasta, klength, rNum, cNum):
+def kmeans(fasta, klength_min, klength_max, rNum, cNum):
     inputData = parseFasta(fasta)
-    temp = virus01.append(inputData)
-    temp = temp.drop_duplicates(keep="last")
+#     temp = virus01.append(inputData)
+#     temp = temp.drop_duplicates(keep="last")
         
-    temp["Sequence"] = temp["Sequence"].apply(lambda x: x.replace("-", ""))
-    kmerXTableInput = kmerXTable(temp, klength, klength)
+    inputData["Sequence"] = inputData["Sequence"].apply(lambda x: x.replace("-", ""))
+    kmerXTableInput = kmerXTable(inputData, klength_min, klength_max)
         
         
     km = KMeans(random_state = rNum, n_clusters = cNum)
@@ -39,18 +41,55 @@ def kmeans(fasta, klength, rNum, cNum):
     y_hat = km.predict(kmerXTableInput)
         
     return y_hat, kmerXTableInput
+        
+def kmeans_semiSupervised(fasta, klength_min, klength_max, rNum, y_hat):
+    inputData = parseFasta(fasta)
+    inputData["Sequence"] = inputData["Sequence"].apply(lambda x: x.replace("-", ""))
+    kmerXTableInput = kmerXTable(inputData, klength_min, klength_max)
+    
+    PCAembedding = PCA(n_components=2)
+    NkmerXTableInput = preprocessing.normalize(kmerXTableInput)
+    PCAembedding_low = PCAembedding.fit_transform(NkmerXTableInput)
+    
+    ms = MeanShift()
+    ms.fit(PCAembedding_low)
+    cluster_centers = ms.cluster_centers_
+
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        kmms = KMeans(init = cluster_centers, n_clusters = len(cluster_centers))
+        kmms_labels = kmms.fit_predict(PCAembedding_low)
+
+    # convert all clusters into two clusters
+    kmerXTableInput["pLabels"] = kmms_labels
+    kmerXTableInput["aLabels"] = y_hat
+    newLabels_clusters_1 = kmerXTableInput[kmerXTableInput["aLabels"] == 1]["pLabels"].tolist()
+    newLabels_clusters_0 = kmerXTableInput[kmerXTableInput["aLabels"] == 0]["pLabels"].tolist()
+    newLabels = []
+
+    for label in kmms_labels:
+        if (label in newLabels_clusters_1) & (label not in newLabels_clusters_0):
+            newLabels.append(1)
+        else:
+            newLabels.append(0)
+            
+    return newLabels, kmerXTableInput.drop(columns=["pLabels", "aLabels"])
     
 def PCA2d(kTable, y_hat, filename):
-    embedding = PCA()
-    embedding.fit(kTable)
-    show = pd.DataFrame(embedding.transform(kTable))
+    embedding = PCA(n_components= 2)
+    nkTable = preprocessing.normalize(kTable)
+    embedding.fit(nkTable)
+    show = pd.DataFrame(embedding.transform(nkTable))
     # show kmeans clustering
     show["labels"] = y_hat
     ax = show[show["labels"]==1].plot.scatter(x=0, y=1, style="o", color="red", s=2)
     show[show["labels"]==0].plot.scatter(x=0, y=1, style="o", color="blue", s=2, ax=ax)
-    red = mpatches.Patch(color='red', label='Fungus Killers')
-    blue = mpatches.Patch(color='blue', label='Fungus Non-Killers')
-    plt.legend(handles=[red, blue])
+    show[(show["labels"]!= 0) & (show["labels"]!= 1) ].plot.scatter(x=0, y=1, style="o", color="gray", s=2, ax=ax)
+    red = mpatches.Patch(color='red', label='Label 1')
+    blue = mpatches.Patch(color='blue', label='Label 0')
+    grey = mpatches.Patch(color='grey', label='Label -1')
+    plt.legend(handles=[red, blue, grey])
     plt.title('PCA Visualization')
     plt.xlabel('First Principal Component')
     plt.ylabel('Second Principal Component')
@@ -65,21 +104,13 @@ def tSNE2d(kTable, y_hat, filename):
     show["labels"] = y_hat
     ax = show[show["labels"]==1].plot.scatter(x=0, y=1, style="o", color="red", s=2)
     show[show["labels"]==0].plot.scatter(x=0, y=1, style="o", color="blue", s=2, ax=ax)
-    red = mpatches.Patch(color='red', label='Fungus Killers')
-    blue = mpatches.Patch(color='blue', label='Fungus Non-Killers')
-    plt.legend(handles=[red, blue])
+    show[(show["labels"]!= 0) & (show["labels"]!= 1) ].plot.scatter(x=0, y=1, style="o", color="gray", s=2, ax=ax)
+    red = mpatches.Patch(color='red', label='Label 1')
+    blue = mpatches.Patch(color='blue', label='Label 0')
+    grey = mpatches.Patch(color='grey', label='Label -1')
+    plt.legend(handles=[red, blue, grey])
     plt.title('tSNE Visualization\n')
     plt.xlabel('First Component')
     plt.ylabel('Second Component')
     plt.savefig('nonNotebookFiles/' + filename + '.png', bbox_inches='tight')
     plt.close()
-    
-# def kmeans_semiSupervised():
-    
-PATH1 = "../data/Sclerotinia_biocontrol_mycovirus_nucleotide.fasta"
-PATH01 = "../data/mycovirus_genbank_all_refseq_nucleotide_unique.fasta"
-
-virus1 = parseFasta(PATH1)
-virus01 = parseFasta(PATH01)
-virus01 = virus01.append(virus1)
-virus01 = virus01.drop_duplicates(keep="last")
